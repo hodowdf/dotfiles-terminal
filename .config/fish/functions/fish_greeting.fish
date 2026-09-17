74aa707de8a6bde420603a5661f30b9b0e09a506
Felipe Ortiz <hodow223@gmail.com>
2026-09-17 16:28:55 -0500

    Update fish_greeting.fish

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-    set -l images $__fish_config_dir/fastfetch/logo/*.jpg
+    set -l images ~/.config/fastfetch/logo/*.jpg
     if command -v fastfetch >/dev/null
         if set -q images[1]
             fastfetch --logo $images[(random 1 (count $images))]
